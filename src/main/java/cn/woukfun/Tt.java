6d40f95f26ff64475b4320e253c8f0577f059c84
kezry <c.jac@foxmail.com>
2020-09-13 11:12:50 +0800

    “pdate .gitignoree

--- src/main/java/cn/woukfun/Tt.java
+++ src/main/java/cn/woukfun/Tt.java
@@ -4,5 +4,9 @@ public class Tt {
     public static void main(String[] args) {
         System.out.println("Hello,TT!");
         System.out.println("fdhsafgawiue");
+        System.out.println("fdhsafgawiue");
+        System.out.println("fdhsafgawiue");
+        System.out.println("fdhsafgawiue");
+        System.out.println("fdhsafgawiue");
     }
 }
